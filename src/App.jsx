d9d7d082d27b8331841005c726d6307f3a5637c9
TomasSorgetti <tomassorgetti456@gmail.com>
2025-08-14 10:45:26 -0300

    feat: loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Routes>
-        {/* Pages */}
-        <Route path="/" element={<HomePage />} />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Routes>
+          {/* Pages */}
+          <Route path="/" element={<HomePage />} />
 
-        {/* Error page */}
-        <Route path="*" element={<h1>Error page</h1>} />
-      </Routes>
+          {/* Error page */}
+          <Route path="*" element={<h1>Error page</h1>} />
+        </Routes>
+      )}
     </>
   );
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Logo from "../assets/images/banner/banner_icon.png";
+
+const Loading = () => {
+  const containerRef = useRef(null);
+  const spinnerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap.fromTo(
+        spinnerRef.current,
+        { opacity: 0, y: 10 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.8,
+          ease: "power2.out",
+          repeat: -1,
+          yoyo: true,
+        }
+      );
+    },
+    { scope: containerRef }
+  );
+
+  return (
+    <div
+      ref={containerRef}
+      className="flex items-center justify-center h-screen"
+    >
+      <div ref={spinnerRef}>
+        <img src={Logo} alt="iescrow logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
