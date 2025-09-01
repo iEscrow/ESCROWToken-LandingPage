f0d0c5e1ee2c50baeab856dbb724990f5aff1745
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-01 20:58:36 -0300

    feat: Bento translations added

--- src/components/Bento.jsx
+++ src/components/Bento.jsx
@@ -1,74 +1,63 @@
 import BentoCard from "./ui/BentoCard";
 import Rocket from "../assets/images/cards/tokenomics.png";
+import { Trans } from "react-i18next";
 
-export default function Bento() {
+export default function Bento({ t }) {
   return (
     <section className="w-full flex flex-col items-center text-center gap-4">
-      <h2 className="text-5xl text-text-primary font-bold flex flex-col items-center text-center gap-4">
-        Engineered for Sustained{" "}
-        <span className="gradient-text">Price Appreciation</span>
+      <h2 className="text-text-primary flex flex-col font-semibold text-xl sm:text-2xl md:text-3xl lg:text-5xl xl:leading-16">
+        <Trans i18nKey="Home.Bento.title">
+          <span className="gradient-text"></span>
+        </Trans>
       </h2>
-      <p className="text-text-secondary max-w-[500px]">
-        Limited presale, deflationary mechanics, and strategic tokenomics drive
-        value for early investors.
+      <p className="text-text-secondary mb-20 mt-4 max-w-[400px] mx-auto lg:max-w-[520px]">
+        {t("Home.Bento.description")}
       </p>
 
-      <div className="w-full mt-10 py-4 bg-no-repeat bg-cover bg-center bg-[url('/images/bento_bg.png')] lg:py-32">
+      <div className="w-full py-4 bg-no-repeat bg-cover bg-center bg-[url('/images/bento_bg.png')] lg:py-32">
         <div className="w-full p-4 container mx-auto grid grid-cols-1 md:grid-cols-5 md:grid-rows-3 gap-4 xl:max-w-[1300px]">
           <BentoCard extraclases="col-span-1 md:col-span-3 ">
             <h3 className="text-2xl font-semibold text-text-primary lg:text-4xl 2xl:text-5xl">
-              Locked Staking
+              {t("Home.Bento.cards.1.title")}
             </h3>
             <p className="text-base leading-9 max-w-[400px]">
-              With this type of staking, holders tend to lock in value for
-              longer periods of time, resulting in better price action and
-              greater yield.
+              {t("Home.Bento.cards.1.description")}
             </p>
           </BentoCard>
 
           <BentoCard extraclases="col-span-1 md:col-span-2 " active>
             <h3 className="text-2xl font-semibold text-text-primary lg:text-4xl 2xl:text-5xl">
-              Locked Staking
+              {t("Home.Bento.cards.2.title")}
             </h3>
             <p className="text-base leading-9">
-              With this type of staking, holders tend to lock in value for
-              longer periods of time, resulting in better price action and
-              greater yield.
+              {t("Home.Bento.cards.2.description")}
             </p>
           </BentoCard>
 
           <BentoCard extraclases="col-span-1 md:col-span-2 ">
             <h3 className="text-2xl font-semibold text-text-primary lg:text-4xl 2xl:text-5xl">
-              Locked Staking
+              {t("Home.Bento.cards.3.title")}
             </h3>
             <p className="text-base leading-9">
-              With this type of staking, holders tend to lock in value for
-              longer periods of time, resulting in better price action and
-              greater yield.
+              {t("Home.Bento.cards.3.description")}
             </p>
           </BentoCard>
 
           <BentoCard extraclases="col-span-1 md:col-span-3">
             <h3 className="text-2xl font-semibold text-text-primary lg:text-4xl 2xl:text-5xl">
-              Locked Staking
+              {t("Home.Bento.cards.4.title")}
             </h3>
             <p className="text-base leading-9">
-              With this type of staking, holders tend to lock in value for
-              longer periods of time, resulting in better price action and
-              greater yield.
+              {t("Home.Bento.cards.4.description")}
             </p>
           </BentoCard>
 
           <BentoCard extraclases="col-span-1 md:col-span-5 ">
             <h3 className="text-2xl font-semibold text-text-primary lg:text-4xl 2xl:text-5xl">
-              Locked Staking
+              {t("Home.Bento.cards.5.title")}
             </h3>
             <p className="text-base leading-9 max-w-[400px] xl:max-w-[600px]">
-              With this type of staking, holders tend to lock in value for
-              longer periods of time, resulting in better price action and
-              greater yield this type of staking, holders tend to lock in value
-              for longer periods of time, resulting in better price action and
-              greater yield.
+              {t("Home.Bento.cards.5.description")}
             </p>
             <img
               src={Rocket}
