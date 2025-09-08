081975103e64fa9bda446a0ef1ebd80107610056
TomasSorgetti <tomassorgetti456@gmail.com>
2025-09-07 22:32:34 -0300

    feat: footer logo added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,141 +1,17 @@
-import Logo from "../assets/images/logo.svg";
+import FooterLogo from "../assets/images/footer.svg";
 import LangFooterSelector from "../features/i18n/components/LangFooterSelector";
-import FooterDropdown from "./ui/FooterDropdown";
-import SocialButton from "./ui/SocialButton";
 
 export default function Footer({ t }) {
-  const company = {
-    title: t("Footer.about.title"),
-    items: [
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.about.about"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.about.terms"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.about.privacy"),
-      },
-    ],
-  };
-
-  const services = {
-    title: t("Footer.services.title"),
-    items: [
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.services.download"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.services.marketplace"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.services.escrow"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.services.program"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.services.application"),
-      },
-    ],
-  };
-
-  const support = {
-    title: t("Footer.support.title"),
-    items: [
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.support.center"),
-      },
-      {
-        href: "/#",
-        target: "_self",
-        label: t("Footer.support.security"),
-      },
-    ],
-  };
-
   return (
     <footer className="w-full px-4 pt-4 border-t-[1px] border-card-border ">
       <div className="container mx-auto flex flex-col items-center gap-4">
-        <div className="flex gap-4 items-start w-full lg:py-8">
-          <a href="#" className="hidden lg:flex">
-            <img
-              src={Logo}
-              alt="iEscrow Logo"
-              draggable="false"
-              loading="eager"
-              className="w-40"
-            />
-          </a>
-
-          <div className="flex flex-col w-full gap-4 md:flex-row md:gap-8 md:justify-between lg:justify-end lg:gap-10 xl:gap-28 2xl:gap-40">
-            <FooterDropdown title={company.title} list={company.items} />
-            <FooterDropdown title={services.title} list={services.items} />
-            <FooterDropdown title={support.title} list={support.items} />
-
-            <div className="w-full md:w-auto">
-              <span className="text-primary uppercase font-semibold">
-                {t("Footer.community")}
-              </span>
-              <div className="flex items-start justify-start gap-7 flex-wrap max-w-[160px] mt-4">
-                <SocialButton
-                  href="https://www.facebook.com/profile.php?id=61580173739629"
-                  title="Facebook"
-                  icon="facebook"
-                  variant="secondary"
-                />
-                <SocialButton
-                  href="https://www.instagram.com/iescrowcrypto"
-                  title="instagram"
-                  icon="instagram_secondary"
-                  variant="secondary"
-                />
-                <SocialButton
-                  href="https://x.com/iEscrowcrypto"
-                  title="Twitter"
-                  icon="twitter_secondary"
-                  variant="secondary"
-                />
-                <SocialButton
-                  href="https://www.tiktok.com/@iescrowcrypto"
-                  title="Tiktok"
-                  icon="tiktok"
-                  variant="secondary"
-                />
-                <SocialButton
-                  href="https://www.youtube.com/@iEscrowcrypto"
-                  title="Youtube"
-                  icon="youtube_secondary"
-                  variant="secondary"
-                />
-                <SocialButton
-                  href="https://t.me/+HByTVekbjfJiMzdh"
-                  title="Telegram"
-                  icon="telegram_secondary"
-                  variant="secondary"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        <img
+          src={FooterLogo}
+          alt="Escrow"
+          draggable="false"
+          loading="lazy"
+          className="w-full mx-auto py-10 lg:max-w-[974px]"
+        />
 
         <div className="w-full text-center flex flex-col-reverse items-center border-t-[1px] border-card-border py-4 lg:flex-row lg:w-full lg:justify-between">
           <p className="text-primary text-sm mt-6 lg:mt-0 lg:w-full lg:text-start">
